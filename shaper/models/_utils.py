3b680cf6b03a7f56d719c283b18e5fb51d5a6f6a
Jiayuan Gu <jigu@ucsd.edu>
2018-11-10 12:20:47 -0800

    add pointnet

---
+++ shaper/models/_utils.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class Conv1dBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1,
+                 relu=True, bn=True, bn_momentum=0.1, **kwargs):
+        super(Conv1dBlock, self).__init__()
+
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size,
+                              bias=(not bn), **kwargs)
+        if bn:
+            self.bn = nn.BatchNorm1d(out_channels, momentum=bn_momentum)
+        else:
+            self.bn = None
+        self.relu = relu
+
+        self.init_weights()
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu:
+            x = F.relu(x, inplace=True)
+        return x
+
+    def init_weights(self):
+        init_uniform(self.conv, self.relu)
+        if self.bn is not None:
+            init_bn(self.bn)
+
+
+class LinearBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 relu=True, bn=True, bn_momentum=0.1):
+        super(LinearBlock, self).__init__()
+
+        self.fc = nn.Linear(in_channels, out_channels, bias=(not bn))
+        if bn:
+            self.bn = nn.BatchNorm1d(out_channels, momentum=bn_momentum)
+        else:
+            self.bn = None
+        self.relu = relu
+
+        self.init_weights()
+
+    def forward(self, x):
+        x = self.fc(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu:
+            x = F.relu(x, inplace=True)
+        return x
+
+    def init_weights(self):
+        if self.bn is not None:
+            init_bn(self.bn)
+
+
+def init_bn(module):
+    if module.weight is not None:
+        nn.init.ones_(module.weight)
+    if module.bias is not None:
+        nn.init.zeros_(module.bias)
+
+
+def init_uniform(module, relu=True):
+    if module.weight is not None:
+        nn.init.kaiming_uniform_(module.weight, nonlinearity='relu' if relu else 'linear')
+    if module.bias is not None:
+        nn.init.zeros_(module.bias)
---
+++ shaper/models/pointnet.py
@@ -0,0 +1,167 @@
+import torch
+import torch.nn as nn
+
+from ._utils import Conv1dBlock, LinearBlock
+
+
+class PointNetLocal(nn.ModuleList):
+    def __init__(self, in_channels,
+                 local_channels=(64, 128, 1024),
+                 bn=True):
+        super(PointNetLocal, self).__init__()
+
+        self.in_channels = in_channels
+
+        for ind, out_channels in enumerate(local_channels):
+            self.append(Conv1dBlock(in_channels, out_channels, relu=True, bn=bn))
+            in_channels = out_channels
+
+        self.out_channels = in_channels
+
+    def forward(self, x):
+        for module in self:
+            x = module(x)
+        return x
+
+
+class PointNetGlobal(nn.ModuleList):
+    def __init__(self, in_channels,
+                 global_channels=(512, 256),
+                 bn=True):
+        super(PointNetGlobal, self).__init__()
+
+        self.in_channels = in_channels
+
+        for ind, out_channels in enumerate(global_channels):
+            self.append(LinearBlock(in_channels, out_channels, relu=True, bn=bn))
+            in_channels = out_channels
+
+        self.out_channels = in_channels
+
+    def forward(self, x):
+        for module in self:
+            x = module(x)
+        return x
+
+
+class TNet(nn.Module):
+    def __init__(self,
+                 in_channels=3,
+                 out_channels=3,
+                 local_channels=(64, 128, 1024),
+                 global_channels=(512, 256),
+                 bn=True):
+        super(TNet, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        # local features
+        self.mlp_local = PointNetLocal(in_channels, local_channels, bn=bn)
+
+        # global features
+        self.mlp_global = PointNetGlobal(self.mlp_local.out_channels, global_channels, bn=bn)
+
+        self.linear = nn.Linear(self.mlp_global.out_channels, in_channels * out_channels, bias=True)
+
+        self.init_weights()
+
+    def forward(self, x):
+        x = self.mlp_local(x)
+        x, _ = torch.max(x, 2)  # [N, C, W]
+        x = self.mlp_global(x)
+        x = self.linear(x)
+        x = x.view(-1, self.out_channels, self.in_channels)
+        I = torch.eye(self.out_channels, self.in_channels, device=x.device)
+        x.add_(I)  # broadcast first dimension
+        return x
+
+    def init_weights(self):
+        # set linear transform be 0
+        nn.init.zeros_(self.linear.weight)
+        nn.init.zeros_(self.linear.bias)
+
+
+class PointNetStem(nn.Module):
+    def __init__(self, in_channels,
+                 stem_channels=(64, 64),
+                 bn=True):
+        super(PointNetStem, self).__init__()
+
+        self.in_channels = in_channels
+
+        # input transform
+        self.transform_input = TNet(in_channels, in_channels, bn=bn)
+
+        # feature stem
+        self.stem = PointNetLocal(in_channels, stem_channels, bn=bn)
+        self.out_channels = self.stem.out_channels
+
+        # feature transform
+        self.transform_stem = TNet(self.out_channels, self.out_channels, bn=bn)
+
+    def forward(self, x):
+        end_points = {}
+
+        # input transform
+        trans_input = self.transform_input(x)
+        x = torch.bmm(trans_input, x)
+        end_points['trans_input'] = trans_input
+
+        # feature stem
+        x = self.stem(x)
+
+        trans_stem = self.transform_stem(x)
+        x = torch.bmm(trans_stem, x)
+        end_points['trans_stem'] = trans_stem
+
+        return x, end_points
+
+
+class PointNetCls(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 stem_channels=(64, 64),
+                 local_channels=(64, 128, 1024),
+                 global_channels=(512, 256),
+                 bn=True):
+        super(PointNetCls, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.stem = PointNetStem(in_channels, stem_channels, bn=bn)
+        self.mlp_local = PointNetLocal(self.stem.out_channels, local_channels, bn=bn)
+        self.mlp_global = PointNetGlobal(self.mlp_local.out_channels, global_channels, bn=bn)
+        self.linear = nn.Linear(self.mlp_global.out_channels, out_channels, bias=False)
+
+        self.init_weights()
+
+    def forward(self, x):
+        x, end_points = self.stem(x)
+
+        x = self.mlp_local(x)
+        x, max_indices = torch.max(x, 2)
+        end_points['key_points'] = max_indices
+        x = self.mlp_global(x)
+        x = self.linear(x)
+
+        return x, end_points
+
+    def init_weights(self):
+        nn.init.kaiming_uniform_(self.linear.weight, nonlinearity='linear')
+
+
+if __name__ == '__main__':
+    batch_size = 32
+    in_channels = 3
+    num_points = 2048
+    num_classes = 10
+
+    data = torch.rand(batch_size, in_channels, num_points)
+    transform = TNet()
+    out = transform(data)
+    print('TNet', out.size())
+
+    pointnet = PointNetCls(in_channels, num_classes)
+    out, _ = pointnet(data)
+    print('pointnet', out.size())
