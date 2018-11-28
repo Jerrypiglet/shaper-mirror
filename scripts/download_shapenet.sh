da48df5b807587697d9539ef29f445469f1d97ff
Maximilian Jaritz <mjaritz@eng.ucsd.edu>
2018-11-28 14:27:24 -0800

    change to shapenet hdf5

--- scripts/download_shapenet.sh
+++ scripts/download_shapenet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
-unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
-rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+wget https://shapenet.cs.stanford.edu/media/shapenet_part_seg_hdf5_data.zip --no-check-certificate
+unzip shapenet_part_seg_hdf5_data.zip
+rm shapenet_part_seg_hdf5_data.zip
+mv hdf5_data shapenet
--- shaper/data/datasets/shapenet.py
+++ shaper/data/datasets/shapenet.py
@@ -1,76 +1,51 @@
 import os.path as osp
-import json
 
 import numpy as np
+import h5py
 
 from torch.utils.data import Dataset
 
 
 class ShapeNet(Dataset):
-    ROOT_DIR = "data/shapenet"
-    cat_file = "synsetoffset2category.txt"
-    split_dir = "train_test_split"
     dataset_map = {
-        "train": "shuffled_train_file_list.json",
-        "val": "shuffled_val_file_list.json",
-        "test": "shuffled_test_file_list.json",
+        "train": "train_hdf5_file_list.txt",
+        "val": "val_hdf5_file_list.txt",
+        "test": "test_hdf5_file_list.txt",
     }
 
-    def __init__(self, root_dir, dataset_names, transform=None,
-                 num_points=-1, shuffle_points=False):
+    def __init__(self, root_dir, dataset_names, transform=None, num_points=-1, shuffle_points=False, load_seg=False):
         self.root_dir = root_dir
-        self.datasets_names = dataset_names
         self.num_points = num_points
         self.shuffle_points = shuffle_points
         self.transform = transform
+        self.load_seg = load_seg
 
-        # classes
-        self.class_to_offset_map = self._load_cat_file()
-        self.offset_to_class_map = {v: k for k, v in self.class_to_offset_map.items()}
-        self.classes = list(self.class_to_offset_map.keys())
-        sorted(self.classes)
-        self.classes_to_ind_map = {c: i for i, c in enumerate(self.classes)}
-
-        # meta data
-        self.meta_data = []
+        # as dataset is small, we can load it into RAM completely
+        hdf5_filenames = []
         for dataset_name in dataset_names:
-            meta_data = self._load_dataset(dataset_name)
-            self.meta_data.extend(meta_data)
-        print("{} classes with {} models".format(len(self.classes), len(self.meta_data)))
-
-    def _load_cat_file(self):
-        class_to_offset_map = {}
-        with open(osp.join(self.root_dir, self.cat_file), 'r') as fid:
-            for line in fid:
-                class_name, class_dir = line.strip().split()
-                class_to_offset_map[class_name] = class_dir
-        return class_to_offset_map
-
-    def _load_pts(self, fname):
-        return np.loadtxt(fname).astype(np.float32)
-
-    def _load_dataset(self, dataset_name):
-        split_fname = osp.join(self.root_dir, self.split_dir, self.dataset_map[dataset_name])
-        fname_list = json.load(open(split_fname, 'r'))
-        meta_data = []
-        for fname in fname_list:
-            # fname = fname.replace("shape_data", self.root_dir)
-            _, offset, token = fname.split("/")
-            pts_path = osp.join(self.root_dir, offset, "points", token + '.pts')
-            class_name = self.offset_to_class_map[offset]
-            data = {
-                'token': token,
-                'class': class_name,
-                'pts_path': pts_path,
-            }
-            meta_data.append(data)
-        return meta_data
+            file_list_txt_path = osp.join(self.root_dir, self.dataset_map[dataset_name])
+            with open(file_list_txt_path, 'r') as f:
+                hdf5_filenames.extend(f.read().splitlines())
+        self.point_clouds = []
+        self.class_labels = []
+        if self.load_seg:
+            self.seg_labels = []
+        for hdf5_filename in hdf5_filenames:
+            f = h5py.File(osp.join(self.root_dir, hdf5_filename))
+            self.point_clouds.append(f['data'][:])
+            self.class_labels.append(f['label'][:])
+            if self.load_seg:
+                self.seg_labels.append(f['pid'][:])
+        self.point_clouds = np.concatenate(self.point_clouds, 0)
+        self.class_labels = np.concatenate(self.class_labels, 0).ravel().astype(int)
+        if self.load_seg:
+            self.seg_labels = np.concatenate(self.seg_labels, 0)
 
     def __getitem__(self, index):
-        meta_data = self.meta_data[index]
-        class_name = meta_data["class"]
-        class_ind = self.classes_to_ind_map[class_name]
-        points = self._load_pts(meta_data["pts_path"])
+        class_label = self.class_labels[index]
+        points = self.point_clouds[index]
+        if self.load_seg:
+            seg_labels = self.seg_labels[index]
 
         if self.shuffle_points:
             choice = np.random.permutation(len(points))
@@ -84,14 +59,21 @@ class ShapeNet(Dataset):
                 pad = np.random.permutation(choice)[:num_pad]
                 choice = np.concatenate([choice, pad])
         points = points[choice]
+        if self.load_seg:
+            seg_labels = self.seg_labels[choice]
 
         if self.transform is not None:
             points = self.transform(points)
 
-        return {
+        out_dict = {
             "points": points,
-            "cls_labels": class_ind,
+            "cls_labels": class_label
         }
 
+        if self.load_seg:
+            out_dict["seg_labels"] = seg_labels
+
+        return out_dict
+
     def __len__(self):
-        return len(self.meta_data)
+        return len(self.class_labels)
