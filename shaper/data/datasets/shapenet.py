f53b89f8ff03fb036dd1306b5a33dafbd1793929
Qinru Li <q4li@eng.ucsd.edu>
2018-12-31 17:22:54 -0800

    [Add]: ShapeNet dataset with normal

--- shaper/data/datasets/shapenet.py
+++ shaper/data/datasets/shapenet.py
@@ -123,7 +123,6 @@ class ShapeNet(Dataset):
         fname_list = json.load(open(split_fname, 'r'))
         meta_data = []
         for fname in fname_list:
-            # fname = fname.replace("shape_data", self.root_dir)
             _, catid, token = fname.split("/")
             pts_path = osp.join(self.root_dir, catid, "points", token + '.pts')
             class_name = self.catid_to_class_map[catid]
@@ -160,6 +159,151 @@ class ShapeNet(Dataset):
                 points, seg_label = self.seg_transform(points, seg_label)
             points.transpose_(0, 1)
 
+        out_dict["points"] = points
+        out_dict["cls_label"] = cls_label
+        if seg_label is not None:
+            out_dict["seg_label"] = seg_label
+
+        return out_dict
+
+    def __len__(self):
+        return len(self.meta_data)
+
+
+class ShapeNetNormal(Dataset):
+    """ShapeNetCore dataset, points include normal
+
+    Each class of ShapeNetCore is assigned a catid/offset, like "02691156".
+    Each part is associated with one class.
+    - 16 object categories (airplane, chair, motorbike)
+    - 50 part classes (each object category has 2-6 part classes)
+
+    Attributes:
+        classes (list): the names of classes
+        class_to_seg_map (dict): mapping from class labels to segmentation labels
+        meta_data (list of dict): meta information of data
+
+    Notes:
+        The seg_file "overallid_to_catid_partid.json" is copied from HDF5 data.
+
+    """
+    URL = "https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip"
+    ROOT_DIR = "../../../data/shapenet_normal"
+    cat_file = "synsetoffset2category.txt"
+    seg_file = "overallid_to_catid_partid.json"
+    split_dir = "train_test_split"
+    dataset_map = {
+        "train": "shuffled_train_file_list.json",
+        "val": "shuffled_val_file_list.json",
+        "test": "shuffled_test_file_list.json",
+    }
+
+    def __init__(self, root_dir, dataset_names, transform=None,
+                 num_points=-1, shuffle_points=False, normalize=True,
+                 load_seg=False, seg_transform=None):
+        """
+
+        Args:
+            root_dir (str): the root directory of data.
+            dataset_names (list of str): the names of dataset, e.g. ["train", "test"]
+            transform (object): methods to transform inputs.
+            num_points (int): the number of input points. -1 means using all.
+            shuffle_points (bool): whether to shuffle input points.
+            normalize (bool): whether to normalize points.
+            load_seg (bool): whether to load segmentation labels.
+            seg_transform (object): methods to transform inputs and segmentation labels.
+
+        """
+        self.root_dir = root_dir
+        self.dataset_names = dataset_names
+        self.num_points = num_points
+        self.shuffle_points = shuffle_points
+        self.normalize = normalize
+        self.transform = transform
+        self.load_seg = load_seg
+        self.seg_transform = seg_transform
+
+        # classes
+        self.class_to_catid_map = self._load_cat_file()
+        self.catid_to_class_map = {v: k for k, v in self.class_to_catid_map.items()}
+        self.classes = list(self.class_to_catid_map.keys())
+        self.class_to_ind_map = {c: i for i, c in enumerate(self.classes)}
+
+        # segid to (catid, partid). Notice that partid start from 1
+        if self.load_seg:
+            self.segid_to_catid_partid_map = self._load_seg_file()
+            self.class_to_seg_map = {}
+            for cls, catid in self.class_to_catid_map.items():
+                class_ind = self.class_to_ind_map[cls]
+                segids = [segid for segid, x in enumerate(self.segid_to_catid_partid_map) if x[0] == catid]
+                self.class_to_seg_map[class_ind] = segids
+
+        # meta data
+        self.meta_data = []
+        for dataset_name in dataset_names:
+            meta_data = self._load_dataset(dataset_name)
+            self.meta_data.extend(meta_data)
+        print("{} classes with {} models".format(len(self.classes), len(self.meta_data)))
+
+    def _load_cat_file(self):
+        class_to_offset_map = OrderedDict()
+        with open(osp.join(self.root_dir, self.cat_file), 'r') as fid:
+            for line in fid:
+                class_name, class_dir = line.strip().split()
+                class_to_offset_map[class_name] = class_dir
+        return class_to_offset_map
+
+    def _load_seg_file(self):
+        return json.load(open(osp.join(self.root_dir, self.seg_file), 'r'))
+
+    def _load_pts(self, fname):
+        return np.loadtxt(fname).astype(np.float32)
+
+    def _load_seg(self, fname):
+        return np.loadtxt(fname).astype(int)
+
+    def _convert_part_to_seg(self, cls_label, part_label):
+        seg_label = part_label.copy()
+        for segid in self.class_to_seg_map[cls_label]:
+            catid, partid = self.segid_to_catid_partid_map[segid]
+            seg_label[seg_label == partid] = segid
+        return seg_label
+
+    def _load_dataset(self, dataset_name):
+        split_fname = osp.join(self.root_dir, self.split_dir, self.dataset_map[dataset_name])
+        fname_list = json.load(open(split_fname, 'r'))
+        meta_data = []
+        for fname in fname_list:
+            _, catid, token = fname.split("/")
+            pts_path = osp.join(self.root_dir, catid, token + '.txt')
+            class_name = self.catid_to_class_map[catid]
+            data = {
+                "token": token,
+                "class": class_name,
+                "pts_path": pts_path,
+            }
+            meta_data.append(data)
+        return meta_data
+
+    def __getitem__(self, index):
+        meta_data = self.meta_data[index]
+        points = self._load_pts(meta_data["pts_path"])
+        class_name = meta_data["class"]
+        cls_label = self.class_to_ind_map[class_name]
+        seg_label = points[:, -1].astype(np.int32) if self.load_seg else None
+
+        if self.normalize:
+            points[:, 0:3] = normalize_points(points[:, 0:3])
+        points = points[:, 0:6]     # Discard the segmentation labels
+
+        points, choice = crop_or_pad_points(points, self.num_points, self.shuffle_points)
+        if self.transform is not None:
+            points = self.transform(points)
+            if seg_label is not None and self.seg_transform is not None:
+                points, seg_label = self.seg_transform(points, seg_label)
+            points.transpose_(0, 1)
+
+        out_dict = {}
         out_dict["points"] = points
         out_dict["cls_label"] = cls_label
         if seg_label is not None:
@@ -311,7 +455,8 @@ class ShapeNetH5(Dataset):
 
 if __name__ == "__main__":
     # shapenet = ShapeNet(ShapeNet.ROOT_DIR, ["test"], load_seg=True)
-    shapenet = ShapeNetH5(ShapeNetH5.ROOT_DIR, ["test"], load_seg=True)
+    # shapenet = ShapeNetH5(ShapeNetH5.ROOT_DIR, ["test"], load_seg=True)
+    shapenet = ShapeNetNormal(ShapeNetNormal.ROOT_DIR, ["test"], load_seg=True)
     print("The number of samples:", shapenet.__len__())
     data = shapenet[0]
     points = data["points"]
@@ -325,4 +470,4 @@ if __name__ == "__main__":
 
     # Visualizer.visualize_points(points)
     part_label = np.asarray([shapenet.segid_to_catid_partid_map[label][1] for label in seg_label])
-    Visualizer.visualize_points_with_labels(points, labels=part_label)
+    Visualizer.visualize_points_with_labels(points[:, 0:3], labels=part_label)
