07d130dcfb492fdb0f452750663ca79efb93c943
root <xuyaokun98@gmail.com>
2019-01-13 01:37:45 +0000

    complete part seg for dgcnn

--- shaper/models/dgcnn/build.py
+++ shaper/models/dgcnn/build.py
@@ -22,10 +22,11 @@ def build_dgcnn(cfg):
         net = DGCNNPartSeg(
             in_channels=cfg.INPUT.IN_CHANNELS,
             out_channels=cfg.DATASET.NUM_CLASSES,
+            num_seg_class=cfg.DATASET.NUM_SEG_CLASSES,
         )
-        loss_fn = DGCNNPartSegLoss(cfg.MODEL.POINTNET.REG_WEIGHT,
-                               cfg.MODEL.POINTNET.CLS_LOSS_WEIGHT,
-                               cfg.MODEL.POINTNET.SEG_LOSS_WEIGHT)
+        loss_fn = DGCNNPartSegLoss(cfg.MODEL.DGCNN.REG_WEIGHT,
+                               cfg.MODEL.DGCNN.CLS_LOSS_WEIGHT,
+                               cfg.MODEL.DGCNN.SEG_LOSS_WEIGHT)
         metric_fn = PartSegMetric(cfg.DATASET.NUM_SEG_CLASSES)
     else:
         raise NotImplementedError()
--- shaper/models/dgcnn/dgcnn_part_seg.py
+++ shaper/models/dgcnn/dgcnn_part_seg.py
@@ -30,7 +30,7 @@ class TNet(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.k = k
-
+        
         self.edge_conv = SharedMLP(2 * in_channels, conv_channels, ndim=2)
         self.mlp_local = SharedMLP(conv_channels[-1], local_channels)
         self.mlp_global = MLP(local_channels[-1], global_channels)
@@ -74,9 +74,10 @@ class DGCNNPartSeg(nn.Module):
     def __init__(self,
                  in_channels,
                  out_channels,
+                 num_seg_class,
                  edge_conv_channels=((64, 64), (64, 64), (64,)),
                  inter_channels= 1024,
-                 global_channels=(192, 256, 128),
+                 global_channels=(256, 256, 128),
                  k=20,
                  dropout_prob=0.6,
                  with_transform=True):
@@ -86,25 +87,27 @@ class DGCNNPartSeg(nn.Module):
         self.out_channels = out_channels
         self.k = k
         self.with_transform = with_transform
+        self.num_gpu = torch.cuda.device_count()
 
         #input transform
         if self.with_transform:
-            self.transform_input = TNet(in_channels, inter_channels, k=k)
+            self.transform_input = TNet(in_channels, in_channels, k=k)
         
         self.mlp_edge_conv = nn.ModuleList()
         for out in edge_conv_channels:
             self.mlp_edge_conv.append(EdgeConvBlock(in_channels, out, k))
-            if type(out) == int:
-                in_channels = out 
-            else: 
-                in_channels = out[-1]
+            in_channels = out[-1]
         
-        self.mlp_local = Conv1d(sum([item[-1] for item in edge_conv_channels]), inter_channels, 1)
-        self.mlp_seg = SharedMLP(in_channels, global_channels, dropout=dropout_prob)
-        self.conv_seg = Conv1d(global_channels[-2], global_channels[-1], 1)
 
-        self.lable_conv = Conv1d(out_channels, 64, 1)
-        self.classifier = nn.Linear(global_channels[-1], out_channels, bias=True)
+        self.lable_conv = Conv2d(16, 64, [1,1])
+
+        self.mlp_local = Conv1d(sum([item[-1] for item in edge_conv_channels]), inter_channels, 1)
+
+        mlp_in_channels = inter_channels + edge_conv_channels[-1][-1] + sum([item[-1] for item in edge_conv_channels])
+        self.mlp_seg = SharedMLP(mlp_in_channels, global_channels[:-1], dropout=dropout_prob)
+        self.conv_seg = Conv1d(global_channels[-2], global_channels[-1], 1)
+        self.seg_logit = nn.Conv1d(global_channels[-1], num_seg_class, 1,bias=True)
+
         self.init_weights()
         set_bn(self, momentum=0.01)
 
@@ -112,46 +115,71 @@ class DGCNNPartSeg(nn.Module):
         #specify computing process
         end_points = {}
         x = data_batch["points"]
-        label = data_batch["label"]
 
+        num_point = x.shape[2]
+      
+        cls_label = data_batch["cls_label"]
+        batch_size = cls_label.size()[0]
+        num_classes = 16
+        # print("output size is {}, data shape is {}, label shape is {}\n".format(self.out_channels, x.size(), cls_label.size()))
+        # print("number of gpu is {}".format(torch.cuda.device_count()))
         if self.with_transform:
             trans_input = self.transform_input(x)
             x = torch.bmm(trans_input, x)
             end_points['trans_input'] = trans_input
+        # print("the size of x is {}".format(x.size()))
 
+
+	# edge convolution for point cloud         
         features = []
         for edge_conv in self.mlp_edge_conv:
             x = edge_conv(x)
             features.append(x)
+        # print("the size of x after edge conv is {}".format(x.size()))
 
+	# concatenate all the feature from each edge convolutional layer 
         x = torch.cat(features, dim=1)
-
+   
+        # go through local mlp 
         x = self.mlp_local(x)
-
         x, max_indice = torch.max(x, 2)
+
         end_points['key_point_inds'] = max_indice
 
-        one_hot_label_expand = one_hot_label_expand.view(x.size()[0], 1, 1, -1) # reshape one hot vector 
-        one_hot_label_expand = self.lable_conv(one_hot_label_expand)
+        # use classification label
+        # print("the real cls label {}".format(cls_label))
+        with torch.no_grad():
+            I = torch.eye(16, dtype=x.dtype, device=x.device)
+            one_hot = I[cls_label]
+            one_hot_expand = one_hot.view(batch_size, num_classes, 1, 1)
 
-        out_max = torch.cat(x, one_hot_label_expand)
-        #change size of out_max
-        x = torch.cat(expand, x)
-
+        one_hot_expand = self.lable_conv(one_hot_expand)
         
+        # print("size of x is {}, size of one_hot_expand is {}".format(x.size(), one_hot_expand.size()))
+        # concatenate information from point cloud and label
+        one_hot_expand = one_hot_expand.view(batch_size, -1)
+        out_max = torch.cat([x, one_hot_expand], dim=1)
+        out_max = out_max.unsqueeze(2).expand(-1, -1, num_point)
+
+        cat_features = torch.cat(features, dim=1)
+        # print("size of x is {}, size of cat_features is {}".format(out_max.size(), cat_features.size()))
+        x = torch.cat([out_max, cat_features], dim=1)
         x = self.mlp_seg(x)
         x = self.conv_seg(x)
-        seg_logit = self.classifier(x)
+        seg_logit = self.seg_logit(x)
         preds = {
             'seg_logit': seg_logit
         }
         preds.update(end_points)
 
+        # print("seg_logit is \n {}, size is {}".format(seg_logit, seg_logit.size()))
+        # print("#"*50)
+        # print("label is \n {}, size is {}".format(data_batch["seg_label"], data_batch["seg_label"].size()))
         return preds
     
     def init_weights(self):
-        nn.init.xavier_uniform_(self.classifier.weight)
-        nn.init.zeros_(self.classifier.bias)
+        nn.init.xavier_uniform_(self.seg_logit.weight)
+        nn.init.zeros_(self.seg_logit.bias)
 
 if __name__ == "__main__":
     batch_size = 4
@@ -168,3 +196,4 @@ if __name__ == "__main__":
     out_dict = dgcnn({"points": data})
     for k, v in out_dict.items():
         print('DGCNN:', k, v.shape)
+
