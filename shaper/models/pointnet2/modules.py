added group normalization on pn2ssg, fixed installation scripts

shaper/models/pointnet2/modules.py
@@ -107,7 +107,9 @@ class PointNetSAModule(nn.Module):
                  num_centroids,
                  radius,
                  num_neighbours,
-                 use_xyz):
+                 use_xyz,
+                 use_bn=True,
+                 use_gn=False):
         super(PointNetSAModule, self).__init__()
 
         self.in_channels = in_channels
@@ -115,7 +117,7 @@ class PointNetSAModule(nn.Module):
 
         if use_xyz:
             in_channels += 3
-        self.mlp = SharedMLP(in_channels, mlp_channels, ndim=2, bn=True)
+        self.mlp = SharedMLP(in_channels, mlp_channels, ndim=2, bn=use_bn, gn=use_gn)
 
         self.sampler = FarthestPointSampler(num_centroids)
         self.grouper = QueryGrouper(radius, num_neighbours, use_xyz=use_xyz)
@@ -159,7 +161,9 @@ class PointNetSAModuleMSG(nn.Module):
                  num_centroids,
                  radius_list,
                  num_neighbours_list,
-                 use_xyz):
+                 use_xyz,
+                 use_bn=True,
+                 use_gn=False):
         super(PointNetSAModuleMSG, self).__init__()
 
         self.in_channels = in_channels
@@ -177,7 +181,7 @@ class PointNetSAModuleMSG(nn.Module):
         self.grouper = nn.ModuleList()
 
         for ind in range(num_scales):
-            self.mlp.append(SharedMLP(in_channels, mlp_channels_list[ind], ndim=2, bn=True))
+            self.mlp.append(SharedMLP(in_channels, mlp_channels_list[ind], ndim=2, bn=use_bn, use_gn=use_gn))
             self.grouper.append(QueryGrouper(radius_list[ind], num_neighbours_list[ind], use_xyz=use_xyz))
 
     def forward(self, xyz, feature=None):
@@ -220,11 +224,13 @@ class PointnetFPModule(nn.Module):
     def __init__(self,
                  in_channels,
                  mlp_channels,
-                 num_neighbors):
+                 num_neighbors,
+                 use_bn=True,
+                 use_gn=False):
         super(PointnetFPModule, self).__init__()
 
         self.in_channels = in_channels
-        self.mlp = SharedMLP(in_channels, mlp_channels, ndim=1, bn=True)
+        self.mlp = SharedMLP(in_channels, mlp_channels, ndim=1, bn=use_bn, gn=use_gn)
         self.interpolator = FeatureInterpolator(num_neighbors)
 
     def forward(self, query_xyz, key_xyz, query_feature, key_feature):

shaper/models/pointnet2/pn2_ssg_part_seg.py
@@ -44,7 +44,9 @@ class PointNet2SSGPartSeg(nn.Module):
                  num_fp_neighbours=(3, 3),
                  seg_channels=(128,),
                  dropout_prob=0.5,
-                 use_xyz=True):
+                 use_xyz=True,
+                 use_bn=True,
+                 use_gn=False):
         """
 
         Args:
@@ -89,20 +91,22 @@ class PointNet2SSGPartSeg(nn.Module):
                                          num_centroids=num_centroids[ind],
                                          radius=radius[ind],
                                          num_neighbours=num_neighbours[ind],
-                                         use_xyz=use_xyz)
+                                         use_xyz=use_xyz,
+                                         use_bn=use_bn,
+                                         use_gn=use_gn)
             self.sa_modules.append(sa_module)
             feature_channels = sa_channels[ind][-1]
 
         # Local Feature Extraction Layer
         if use_xyz:
             feature_channels += 3
-        self.mlp_local = SharedMLP(feature_channels, local_channels, bn=True)
+        self.mlp_local = SharedMLP(feature_channels, local_channels, bn=use_bn, gn=use_gn)
 
         # Local Feature Propagation Layer
         inter_channels = [in_channels if use_xyz else in_channels - 3]
         inter_channels[0] += num_classes  # concat with one-hot
         inter_channels.extend([x[-1] for x in sa_channels])
-        self.mlp_local_fp = SharedMLP(local_channels[-1] + inter_channels[-1], fp_local_channels, bn=True)
+        self.mlp_local_fp = SharedMLP(local_channels[-1] + inter_channels[-1], fp_local_channels, bn=use_bn, gn=use_gn)
 
         # Feature Propagation Layers
         self.fp_modules = nn.ModuleList()
@@ -110,7 +114,8 @@ class PointNet2SSGPartSeg(nn.Module):
         for ind in range(num_fp_layers):
             fp_module = PointnetFPModule(in_channels=feature_channels + inter_channels[-2 - ind],
                                          mlp_channels=fp_channels[ind],
-                                         num_neighbors=num_fp_neighbours[ind])
+                                         num_neighbors=num_fp_neighbours[ind],
+                                         use_bn=use_bn, use_gn=use_gn)
             self.fp_modules.append(fp_module)
             feature_channels = fp_channels[ind][-1]
 

shaper/nn/modules/mlp.py
@@ -10,7 +10,8 @@ class MLP(nn.ModuleList):
                  mlp_channels,
                  dropout=None,
                  bn=True,
-                 bn_momentum=0.1):
+                 bn_momentum=0.1,
+                 gn=False):
         """Multilayer perceptron
 
         Args:
@@ -27,7 +28,7 @@ class MLP(nn.ModuleList):
 
         for ind, out_channels in enumerate(mlp_channels):
             self.append(FC(in_channels, out_channels,
-                           relu=True, bn=bn, bn_momentum=bn_momentum))
+                           relu=True, bn=bn, bn_momentum=bn_momentum, gn=gn))
             in_channels = out_channels
 
         self.dropout = nn.Dropout(dropout) if dropout else None
@@ -55,6 +56,7 @@ class SharedMLP(nn.ModuleList):
                  dropout=None,
                  bn=True,
                  bn_momentum=0.1,
+                 gn=False,
                  init_fn=None):
         """Multilayer perceptron shared on resolution (1D or 2D)
 
@@ -82,7 +84,7 @@ class SharedMLP(nn.ModuleList):
 
         for ind, out_channels in enumerate(mlp_channels):
             self.append(mlp_module(in_channels, out_channels, 1,
-                                   relu=True, bn=bn, bn_momentum=bn_momentum))
+                                   relu=True, bn=bn, bn_momentum=bn_momentum, gn=gn))
             in_channels = out_channels
 
         self.out_channels = in_channels