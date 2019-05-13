fixed rotation

shaper/data/datasets/evaluator.py
@@ -267,9 +267,8 @@ def evaluate_part_instance_segmentation(dataset,
             ious= temp
         aps[i]=ap
         print('ap %d'%(i*5+5), ap)
-        break
     print('mean ap', np.mean(aps))
-    #return aps
+    return aps
 
     gen_visu(os.path.join(output_dir,vis_dir), dataset, pred_logits, conf_logits, ious)
     exit(0)

shaper/models/foveal_loss.py
@@ -116,7 +116,7 @@ class ProposalLoss(nn.Module):
 
         v3 = torch.cross(v1,v2)
 
-        rotation_mask = torch.cat([rotation_mask, v3.unsqueeze(-1)] , -1)
+        rotation_mask = torch.cat([v1.unsqueeze(-1), v2.unsqueeze(-1), v3.unsqueeze(-1)] , -1)
         labels['rotation_mask']=rotation_mask
 
 

tools/test_foveal_seg.py
@@ -139,16 +139,17 @@ def test(cfg, output_dir=""):
 
                 if zoom_iteration == 0:
                     proposal_preds = proposal_model(data_batch, 'points')
+                    proposal_loss_dict = proposal_loss_fn(proposal_preds, data_batch)
 
                     proposal_mask = proposal_preds['mask_output'][:,0,:]
+                    ins_seg_label = data_batch['ins_seg_label']
+                    ins_seg_label, _ = torch.max(ins_seg_label, 1)
                     proposal_mask = F.softmax(proposal_mask,1)
                     #radius_mask = proposal_preds['mask_output'][:,1,:]
                     radius_mask = proposal_preds['mask_output'][:,1:4,:]
                     radius_mask = torch.exp(radius_mask)
                     radius_mask = torch.transpose(radius_mask, 2, 1)
-                    #radius_mask = data_batch['radius']
-                    rotation_mask = data_batch['rotation_mask']
-                    #rotation_mask = data_batch['rotation']
+                    radius_mask = data_batch['radius']
                     m,_ = torch.max(proposal_mask, 1, keepdim=True)
                     master_proposal_mask = proposal_mask
                     proposal_mask[proposal_mask < 0.5*m]=0
@@ -178,7 +179,12 @@ def test(cfg, output_dir=""):
                 centroids = centroids.expand(-1, -1, 3, 3)
 
                 gathered_radius = radius_mask.detach().gather(1,centroids[:,:,:,0])
-                gathered_rotation = rotation_mask.detach().gather(1,centroids)
+                gathered_rotation = data_batch['rotation'].detach().gather(1,centroids)
+                gathered_rotation2 = data_batch['rotation_mask'].detach().gather(1,centroids)
+
+                print('gt', iteration, zoom_iteration, gathered_rotation)
+                print('pred', iteration, zoom_iteration, gathered_rotation2)
+
 
 
                 gathered_rotation = torch.squeeze(gathered_rotation, 1)
@@ -192,15 +198,15 @@ def test(cfg, output_dir=""):
                 for crop_size in range(1):#1,5):
                     crop_size=2.5
                     for b in range(batch_size):
-                        while True:
+                        for i in range(20):
                             c0 = (transformed_points[b,:,0]**2)**0.5 < crop_size * gathered_radius[b,:,0]
                             c1 = (transformed_points[b,:,1]**2)**0.5 < crop_size * gathered_radius[b,:,1]
                             c2 = (transformed_points[b,:,2]**2)**0.5 < crop_size * gathered_radius[b,:,2]
                             nearest_indices_temp = torch.nonzero(c0*c1*c2)
                             if nearest_indices_temp.shape[0] >= num_point:
+                                nearest_indices[b] = nearest_indices_temp[:num_point,0].cuda()
                                 break
                             crop_size*=1.05
-                        nearest_indices[b] = nearest_indices_temp[:num_point,0].cuda()
 
                     #zoomed_points = full_points.gather(2, nearest_indices.view(batch_size, 1, crop_size).expand(batch_size, 3, crop_size))
                     #zoomed_points = full_points.gather(2, nearest_indices.view(batch_size, 1, crop_size).expand(batch_size, 3, crop_size))
@@ -267,7 +273,6 @@ def test(cfg, output_dir=""):
                 global_seg_logit_all[zoom_iteration].append((pm/(vm+1e-12)).cpu().numpy())
 
 
-                    #proposal_loss_dict = proposal_loss_fn(proposal_preds, data_batch)
 
 
 
@@ -332,7 +337,7 @@ def main():
     if output_dir:
         config_path = osp.splitext(args.config_file)[0]
         config_path = config_path.replace("configs", "outputs")
-        output_dir = output_dir.replace('@', config_path+'_0')
+        output_dir = output_dir.replace('@', config_path+'')
         mkdir(output_dir)
 
     logger = setup_logger("shaper", output_dir, prefix="test")

tools/test_part_ins_seg.py
@@ -6,6 +6,7 @@ import argparse
 import os.path as osp
 import logging
 import time
+import os
 
 import numpy as np
 import torch
@@ -204,14 +205,13 @@ def main():
     purge_cfg(cfg)
     #cfg.freeze()
 
+
     output_dir = cfg.OUTPUT_DIR
-    # Replace '@' with config path
     if output_dir:
         config_path = osp.splitext(args.config_file)[0]
         config_path = config_path.replace("configs", "outputs")
-        output_dir = output_dir.replace('@', config_path+'_0')
+        output_dir = output_dir.replace('@', config_path)
         mkdir(output_dir)
-
     logger = setup_logger("shaper", output_dir, prefix="test")
     logger.info("Using {} GPUs".format(torch.cuda.device_count()))
     logger.info(args)
@@ -220,22 +220,33 @@ def main():
     logger.info("Running with config:\n{}".format(cfg))
 
     assert cfg.TASK == "part_instance_segmentation"
-    aps = np.zeros((1, 20))
-    for i in range(360, 410, 10):
-        print(i)
-        cfg.TEST.WEIGHT='@/model_%03d.pth'%i
-        aps[(i-360)//10] = test(cfg, output_dir)
-        print(aps[(i-360)//10])
-        break
+    aps = np.zeros((25, 20))
+    output_log = open(os.path.join(output_dir, 'evaluation.txt'), 'w')
+
+    for init in range(5):
+        # Replace '@' with config path
+        for i in range(360, 410, 10):
+            cfg.TEST.WEIGHT='@/model_%03d.pth'%i
+            aps[init*5 + (i-360)//10] = test(cfg, output_dir+'_%d'%init)
+            print(aps[(i-360)//10])
+            output_log.write('Init %d Epoch %d:  %s\n'%(init, i,str(aps[(i-360)//10])))
+            output_log.flush()
+
+    output_log.write('\nFINAL RESULTS\n\n')
     temp = np.mean(aps, 0, keepdims=True)
     std_dev = np.mean((aps - temp)**2,0)**0.5
     print('mean',list(zip(range(5,105,5),np.mean(aps,0))))
+    output_log.write('mean: %s\n'%str(list(zip(range(5,105,5),np.mean(aps,0)))))
     print('std_dev', list(zip(range(5,105,5), std_dev)))
+    output_log.write('std_dev: %s\n'%str(list(zip(range(5,105,5), std_dev))))
     aps = np.mean(aps,1)
     mean = np.mean(aps)
     std_dev = np.mean((aps - mean)**2)**0.5
     print('mean', mean)
+    output_log.write('mean: %s\n'%str(mean))
     print('std dev', std_dev)
+    output_log.write('std dev: %s\n'%str( std_dev))
+    output_log.close()
 
 
 if __name__ == "__main__":